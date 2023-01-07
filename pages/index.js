7daab3aa43e222f55449443351d6b3de22c2f1c5
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-01-06 21:16:13 -0500

    added a navbar using react, tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import NavBar from '../components/navbar'
+import NavBar from '../components/NavBar/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
