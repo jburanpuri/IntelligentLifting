af2ad312b8960b336ed5dbf83b13cf4982262876
Janmejay Buranpuri <60244493+jburanpuri@users.noreply.github.com>
2023-01-12 23:16:44 -0500

    Fixed a typo

--- components/HeroComponent/hero.js
+++ components/HeroComponent/hero.js
@@ -6,11 +6,11 @@ export default function Hero() {
             <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
                 <div className="text-center">
                     <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-                        <span className="block">Make Bigger and Stronger, Faster</span>
-                        <span className="block">Better Plans, For $0</span>
+                        <span className="block">Get Bigger and Stronger, Faster.</span>
+                        <span className="block">For an expensive price of $0</span>
                     </h2>
                     <p className="mt-4 max-w-2xl text-xl text-gray-300 mx-auto">
-                        Training plans don't have to be expensive. Personal coaching isn't neccessary. 
+                        Training plans don't have to be expensive.
                         We believe fitness can change the world, and our platform will provide you with everything you need to get started.
                         Our plans are not simple cookie-cutter plans like you see on other sites. Our plans adabt to your body, To find out more, click below.
                     </p>
