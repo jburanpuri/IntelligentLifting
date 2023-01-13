dd813ec89f2c1ee735111d76330a0da9e75c2c60
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-01-12 20:23:14 -0500

    Added a banner to show schedule via github

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,15 @@ import { Inter } from '@next/font/google'
 import NavBar from '../components/NavbarComponent/navbar'
 import Hero from '../components/HeroComponent/hero'
 import Marketing from '../components/MarketingComponent/marketing'
-import Product from '../components/ProductComponent/products'
 import Footer from '../components/FooterComponent/footer'
+import Banner from '../components/BannerComponent/banner'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+    <Banner/>
     <NavBar />
     <Hero />
     <Marketing />
