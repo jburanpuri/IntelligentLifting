ce3e890a7c248d6e52c346d1b37f08497005e47d
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-01-14 19:55:45 -0500

    replaced <img> with <image> to optomize for next

--- components/ProductComponent/products.js
+++ components/ProductComponent/products.js
@@ -47,11 +47,7 @@ const products = [
             {products.map((product) => (
               <div key={product.id} className="group relative">
                 <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                  />
+   
                 </div>
                 <div className="mt-4 flex justify-between">
                   <div>
