2809eb043518dff7a875b667ee723f41172a22bf
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-01-20 11:34:35 -0500

    Testing layout

--- components/FooterComponent/footer.js
+++ components/FooterComponent/footer.js
@@ -2,27 +2,21 @@ import React from "react";
 
 export default function Footer() {
     return (
-<footer class="p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-900">
-    <div class="sm:flex sm:items-center sm:justify-between content-center">
-        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">Lower Body Specialized</a>
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6 ">Upper Body Specialized</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline">Full Body Focused</a>
-            </li>
-        </ul>
-    </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"><a href="https://flowbite.com/" class="hover:underline">Intelligent Lifting</a>. All Rights Reserved.
-    </span>
-</footer>
 
-    )};
+<footer class="p-4 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 flex flex-col">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="intelligentlifting.com" class="hover:underline">Intelligent Lifting</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 ">Home</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">All Plans</a>
+        </li>
+    </ul>
+</footer>
+)};
 
