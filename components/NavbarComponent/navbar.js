4fe4f96001954b5e857dfd26291c3d9fe70a0028
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-01-21 12:59:42 -0500

    Made nav mobile friendly

--- components/NavbarComponent/navbar.js
+++ components/NavbarComponent/navbar.js
@@ -1,87 +1,76 @@
-import { useState } from "react";
-import Link from "next/link";
+import { useState } from 'react'
+import Link from 'next/link'
 
-export default function NavBar() {
-    const [navbar, setNavbar] = useState(false);
-    const [navbarOpen, setNavbarOpen] = useState(true);
+function NavLink({to, children}) {
+    return <a href={to} className={`mx-4`}>
+        {children}
+    </a>
+}
 
-    function handleToggle() {
-        setNavbarOpen(!navbarOpen);
-    }
-
+function MobileNav({open, setOpen}) {
     return (
-        
-        <nav className={"w-full bg-black shadow top-0"}>
-            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-                <div>
-                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                        <Link href="/home">
-                            <h2 className="text-white text-2xl font-bold">Intelligent Lifting</h2>
-                        </Link>
-                        <div className="md:hidden">
-                            <button
-                                className="p-2 text-white rounded-md outline-none focus:border-white focus:border"
-                                onClick={() => setNavbar(!navbar)}
-                            >
-                                {navbar ? (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                        />
-                                    </svg>
-                                )}
-                            </button>
-                        </div>
-                    </div>
+        <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+            <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20"> 
+                <Link className="text-xl font-semibold" href="/home">Intelligent Lifting</Link>
+            </div>
+            <div className="flex flex-col ml-4">
+                <Link className="text-xl font-medium my-4" href="/home" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Home
+                </Link>
+                <Link className="text-xl font-normal my-4" href="/plans" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    All Plans
+                </Link>
+                <Link className="text-xl font-normal my-4" href="/threeday" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Three Day Plans
+                </Link>
+                <Link className="text-xl font-normal my-4" href="/fourday" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Four Day Plans
+                </Link>
+                <Link className="text-xl font-normal my-4" href="/fiveday" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                    Five Day Plans
+                </Link>
+            </div>  
+        </div>
+    )
+}
+
+export default function Navbar() {
+
+    const [open, setOpen] = useState(false)
+    return (
+        <nav className="flex filter drop-shadow-md bg-black px-4 py-4 h-20 items-center">
+            <MobileNav open={open} setOpen={setOpen}/>
+            <div className="w-3/12 flex items-center">
+                <Link className="text-2xl font-semibold text-white" href="/home">INTELLIGENT LIFTING</Link>
+            </div>
+            <div className="w-9/12 flex justify-end items-center">
+
+                <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden text-white bg-black" onClick={() => {
+                    setOpen(!open)
+                }}>
+                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "bg-black rotate-45 translate-y-3.5" : "bg-black"}`} />
+                    <span className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "bg-black w-0" : "w-full bg-black"}`} />
+                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "bg-black -rotate-45 -translate-y-3.5 " : "bg-black"}`} />
                 </div>
-                <div>
-                    <div
-                        className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"} `}      
-                    >
 
-                    <div onClick={() => setNavbarOpen(false)}>
-                    <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                        <li className="text-white hover:bg-indigo-500 rounded-md">
-                        <Link href="/plans" className="px-4 py-4 text-white hover:bg-indigo-500 rounded-md">All Plans</Link>
-                        </li>
-                        <li className="text-white hover:bg-indigo-500 rounded-md">
-                        <Link href="/threeday" className="px-4 py-4 text-white hover:bg-indigo-500 rounded-md">Three Day Plans</Link>
-                        </li>
-                        <li className="text-white hover:bg-indigo-500 rounded-md">
-                        <Link href="/fourday" className="px-4 py-4 text-white hover:bg-indigo-500 rounded-md">Four Day Plans</Link>
-                        </li>
-                        <li className="text-white hover:bg-indigo-500 rounded-md">
-                        <Link href="/fiveday" className="px-4 py-4 text-white hover:bg-indigo-500 rounded-md">Five Day Plans</Link>
-                        </li>
-                    </ul>
-                    </div>
-
-
-                    </div>
+                <div className="hidden md:flex">
+                    <Link href="/home" className="px-4 py:4 text-xl font-normal	text-white">
+                        Home
+                    </Link>
+                    <Link href="/plans" className="px-4 py:4 text-xl font-normal text-white">
+                        All Plans
+                    </Link>
+                    <Link href="/threeday" className="px-4 py:4 text-xl font-normal text-white">
+                        Three Day Plans
+                    </Link>
+                    <Link href="/fourday" className="px-4 py:4 text-xl font-normal text-white">
+                        Four Day Plans
+                    </Link>
+                    <Link href="/fiveday" className="px-4 py:4 text-xl font-normal text-white">
+                        Five Day Plans
+                    </Link>
                 </div>
             </div>
         </nav>
-    );
+    )
 }
