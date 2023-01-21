e4a525cb97214f710634e446c8ac536d9942e33f
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-01-21 18:00:02 -0500

    fixed component not showing on homepage

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Hero from '../components/HeroComponent/hero'
 import Marketing from '../components/MarketingComponent/marketing'
+import Marketing2 from '../components/MarketingComponent/marketing2'
 
 
 export default function Home() {
    return (
     <>
       <Hero/>
+      <Marketing2/>
       <Marketing/>
     </>
     
