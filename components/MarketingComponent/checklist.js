5bc50ff99dbcff73f89976751d817feecfd72325
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-01-22 23:41:27 -0500

    Added more information to landing page

--- components/MarketingComponent/checklist.js
+++ components/MarketingComponent/checklist.js
@@ -23,23 +23,31 @@ export default function Marketing1() {
 <ul class="mb-8 space-y-4 text-left text-black dark:text-black py-4 ">
     <li class="flex items-center space-x-3">
         <svg class="flex-shrink-0 w-5 h-5 text-primary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-        <span className=" text-lg text-gray-800">Train at your Maximum Adaptive Volume</span>
+        <span className=" text-lg text-gray-800">Train at your Maximum Adaptive Volume (MAV)</span>
     </li>
     <li class="flex items-center space-x-3">
         <svg class="flex-shrink-0 w-5 h-5 text-primary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-        <span className="text-lg text-gray-800">7 Weeks of free training, and can re-run unlimited times</span>
+        <span className="text-lg text-gray-800">7 Weeks of training and re-run unlimited times</span>
     </li>
+    <li class="flex items-center space-x-3">
+        <svg class="flex-shrink-0 w-5 h-5 text-primary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+        <span className="text-lg text-gray-800">Full of supersets and dropsets to keep training fun with high intensity</span>
+    </li>   
     <li class="flex items-center space-x-3">
         <svg class="flex-shrink-0 w-5 h-5 text-primary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
         <span className="text-lg text-gray-800">Absolutely free, no strings attached. No emails, no credit cards</span>
     </li>
     <li class="flex items-center space-x-3">
         <svg class="flex-shrink-0 w-5 h-5 text-primary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-        <span className="text-lg text-gray-800">Continuously improving and expanding our workout plans to provide the best options for achieving your fitness goals</span>
+        <span className="text-lg text-gray-800">Continuously optimizing the workout plans to improve usefulness and effectiveness</span>
     </li>
     <li class="flex items-center space-x-3">
         <svg class="flex-shrink-0 w-5 h-5 text-primary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-        <span className="text-lg text-gray-800">Always open to feedback to improve the website and plans we offer</span>
+        <span className="text-lg text-gray-800">Always increasing selections for workout plans based on demand</span>
+    </li>
+    <li class="flex items-center space-x-3">
+        <svg class="flex-shrink-0 w-5 h-5 text-primary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+        <span className="text-lg text-gray-800">No 3 sets of all exercises, have the sets automatically adjust based on the exercise</span>
     </li>   
 </ul>
 
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Hero from '../components/HeroComponent/hero'
-import Marketing from '../components/MarketingComponent/marketing'
-import Marketing2 from '../components/MarketingComponent/marketing2'
+import Checklist from '../components/MarketingComponent/checklist'
+import Box from '../components/MarketingComponent/box'
 
 
 export default function Home() {
    return (
     <>
       <Hero/>
-      <Marketing2/>
-      <Marketing/>
+      <Box/>
+      <Checklist/>
     </>
     
   )
