b163ad84dd0e6d942e224a13345f0e784fbfff43
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-01-22 23:54:39 -0500

    UPDATED rest of the plan details

--- pages/threeday.js
+++ pages/threeday.js
@@ -5,9 +5,9 @@ import UpperBody from "../components/PlansComponent/ThreeDayPlans/upperfocused"
 export default function ThreeDay() {
     return (
         <>
-        <div className="bg-gray-300 py-4">
-        <h2 className="text-l sm:text-3xl md:text-4xl lg:text-5xl xl:text-4xl text-gray-800 font-black leading-7 md:leading-10 py-2 px-4">
-        <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-purple-600 to-indigo-700">THREE DAY ROUTINES</span>        </h2>
+        <div className="bg-gray-900 py-4">
+        <h2 className="text-center text-3xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-5xl font-black leading-7 md:leading-10">
+        <span className="font-extrabold text-transparent bg-clip-text text-primary">THREE DAY ROUTINES</span>        </h2>
         </div>
             <FullBody />
             <UpperBody />
