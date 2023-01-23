a1b50fcafea87863dfaa1e1dd6c740d019f6c36d
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-01-22 22:49:24 -0500

    testing new gradient

--- components/HeroComponent/hero.js
+++ components/HeroComponent/hero.js
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Hero() {
     return (
       <div>
-<section style={{backgroundImage: "url('/bg.png')"}} className="flex flex-col min-h-screen">
+<section className="flex flex-col min-h-screen bg-gradient-to-t from-gray-800 to-black">
             <div className="flex-1 flex items-center">
             <div className="text-center mx-auto">
-              <h1 className="font-extrabold text-7xl text-primary ">Intelligent Lifting</h1>
+              <h1 className="font-extrabold text-7xl text-primary">Intelligent Lifting</h1>
               <p className="px-4 font-normal text-primary text-4xl mt-5 ">The place to get the best workout plans, for free</p>
-              <Link className="px-6 py-3 text-xl inline-block rounded-md bg-primary text-white hover:bg-cyan-400 transition-colors mt-10 " href="/plans">See Plans</Link>
+              <Link className="px-6 py-3 text-xl inline-block rounded-md bg-primary text-black hover:bg-cyan-400 transition-colors mt-10 " href="/plans">See Plans</Link>
             </div>
           </div>
         </section>
