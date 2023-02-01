40e7c8bcb63d6174dd6a103eb3b30f54ad419421
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-02-01 12:44:04 -0500

    fixed phone nav issue

--- components/NavbarComponent/navbar.js
+++ components/NavbarComponent/navbar.js
@@ -4,24 +4,24 @@ import Link from 'next/link'
 function MobileNav({open, setOpen}) {
     return (
         <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
-            <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20"> 
-                <Link className="text-lg font-semibold" href="/">Intelligent Lifting</Link>
-            </div>
-            <div className="flex flex-col ml-4">
-                <Link className="text-3xl font-normal my-4" href="/" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Home
-                </Link>
-                <Link className="text-3xl font-normal my-4" href="/plans" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Free Plans
-                </Link>
-                <Link className="text-3xl font-normal my-4" href="/custom" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Custom Plans
-                </Link>
-                <Link className="text-3xl font-normal my-4" href="/learningcenter" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Learning Center
-                </Link>
-            </div>  
+        <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20"> 
+            <Link className="text-lg font-semibold" href="/">Intelligent Lifting</Link>
         </div>
+        <div className="flex flex-col ml-4">
+            <Link className="text-3xl font-normal my-4" href="/" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                Home
+            </Link>
+            <Link className="text-3xl font-normal my-4" href="/plans" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                Plans
+            </Link>
+            <Link className="text-3xl font-normal my-4" href="/custom" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                Custom Plans
+            </Link>
+            <Link className="text-3xl font-normal my-4" href="/learningcenter" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
+                Learning Center
+            </Link>
+        </div>  
+    </div>
     )
 }
 export default function Navbar() {
