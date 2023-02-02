cca9689aed7fc75eb13bc13920ea17c0c4510628
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-02-01 20:39:38 -0500

    new info for custom coaching page

--- pages/custom.js
+++ pages/custom.js
@@ -21,11 +21,55 @@ export default function Custom() {
   </div>
 </section>
 
+<section>
+	<div className="container flex flex-col-reverse mx-auto lg:flex-row">
+		<div className="flex flex-col px-6 py-24 space-y-12 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-1/2 text-white">
+			<div className="flex space-x-2 sm:space-x-4">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 w-6 h-6">
+					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+				</svg>
+				<div className="space-y-2">
+					<p className="text-lg font-medium leading-snug">Fully Customized</p>
+					<p className="leading-snug">Experience the exceptional quality that you've come to know and love from our free plans, elevated to new heights with a personal aspect</p>
+				</div>
+			</div>
+			<div className="flex space-x-2 sm:space-x-4">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 w-6 h-6">
+					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+				</svg>
+				<div className="space-y-2">
+					<p className="text-lg font-medium leading-snug">Unique For Everyone</p>
+					<p className="leading-snug">Want to focus on glutes and shoulders? Chest and hamstrings? We got you! The plan will be completely unique to you</p>
+				</div>
+			</div>
+			<div className="flex space-x-2 sm:space-x-4">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 w-6 h-6">
+					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+				</svg>
+				<div className="space-y-2">
+					<p className="text-lg font-medium leading-snug">Tailoried with Science</p>
+					<p className="leading-snug">We don't use bro-science. We use scientific principles to make our plans stand out. 
+          We also make them more fun than any plan on the market. Hard work, consistency, and a good plan will transform your physique</p>
+				</div>
+        <div className="space-y-2">
+					<p className="text-lg font-medium leading-snug">Perfect for Natural Lifters</p>
+					<p className="leading-snug">Natural lifters need to dial in sleep, training, and diet. We can help you do all three with our complete wellness plan</p>
+				</div>
+			</div>
+		</div>
+		<div className="lg:w-1/2 xl:w-1/2 ">
+			<div className="flex items-center justify-center p-4 md:p-8 lg:p-12">
+				<img src="/transformation.png" alt="" className="rounded-lg shadow-lg bg-gray-500 aspect-video sm:min-h-96" />
+			</div>
+		</div>
+	</div>
+</section>
+
     <div>
 <section>
   <div className="bg-gray-900 text-white py-12">
     <div className="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-24">
-      <div className="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
+      <div className="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-5 md:mt-12 px-8">
         <p className="ml-2 text-primary uppercase tracking-loose">Approximate Timeline</p>
         <p className="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">Great Products Delivered Fast</p>
         <p className="text-sm md:text-base text-gray-50 mb-4">
