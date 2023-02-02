d8c496bdbea337cc2d604a7a66f1e9edfe93b80d
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-02-01 21:52:54 -0500

    nav bar issue fix

--- components/NavbarComponent/navbar.js
+++ components/NavbarComponent/navbar.js
@@ -39,9 +39,9 @@ export default function Navbar() {
                 <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden text-white bg-black rounded-lg" onClick={() => {
                     setOpen(!open)
                 }}>
-                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "bg-black rotate-45 translate-y-3.5" : ""}`} />
-                    <span className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "bg-black w-0" : "w-full bg-black"}`} />
-                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "bg-black -rotate-45 -translate-y-3.5 " : ""}`} />
+                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "bg-white rotate-45 translate-y-3.5" : "bg-white"}`} />
+                    <span className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "bg-white w-0" : "w-full"}`} />
+                    <span className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "bg-white -rotate-45 -translate-y-3.5 " : "bg-white"}`} />
                 </div>
 
                 <div className="hidden md:flex">
