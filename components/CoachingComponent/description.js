dc74bc148735cf15d6881bbe41c611d14f5d5470
Janmejay Buranpuri <jburanpu@uwo.ca>
2023-05-03 13:52:54 +1000

    updated title

--- components/CoachingComponent/description.js
+++ components/CoachingComponent/description.js
@@ -5,7 +5,7 @@ const Description = () => {
         <section>
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">Personal Coaching</h1>
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">Custom Training</h1>
             <p className="max-w-2xl mb-6 font-large text-gray-200 lg:mb-8 md:text-xl lg:text-2xl">By simply filling out our comprehensive questionnaire, we'll use your unique fitness profile to create a personalized workout plan that takes into account your goals, physical abilities, schedule, preferred exercises, and available equipment. 
             Say goodbye to generic workout routines and hello to a customized plan that's all about you! In addition, we offer a wellness package to help transform your life even further!</p>
            <a href="#buy" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-primary hover:bg-white focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
