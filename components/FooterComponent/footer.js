20164c2c0c04ec2f4420359ee65ea902fd3281e2
Janmejay Buranpuri <60244493+jburanpuri@users.noreply.github.com>
2024-09-13 19:48:15 -0400

    Update footer.js
    updated the year

--- components/FooterComponent/footer.js
+++ components/FooterComponent/footer.js
@@ -43,7 +43,7 @@ export default function Footer() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-normal text-white font-semibold py-1">
-                Copyright © <span id="get-current-year">2023</span><Link href="/" className="text-white hover:text-primary"> Intelligent Lifting by
+                Copyright © <span id="get-current-year">2024</span><Link href="/" className="text-white hover:text-primary"> Intelligent Lifting by
                 </Link><Link href="/" className="text-white hover:text-primary"> Sonu Lifts</Link>.
               </div>
             </div>
